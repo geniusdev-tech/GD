6744f7e03b052f6e53b6404eade5bbbbed43595d
geniusdev-tech <geniusdev.tech@proton.me>
2023-02-13 08:29:39 -0300

    main

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
           className={scroll ? "header active" : "header"}>
           <div className="Nav_container">
               <div className="logo">
-                    <h3>GDev</h3>
+                    <h3>GD</h3>
               </div>
               <ul
                       className="nav_links"
